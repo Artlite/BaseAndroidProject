ff65aa8fa0b39152ee3b73dcd1fc990ebba99e68
dlernatovich <dlernatovich@geeksforless.net>
2018-11-14 14:29:25 +0200

    Minor updates

--- BSProject/bslibrary/src/main/java/com/artlite/bslibrary/helpers/contact/BSContactHelper.java
+++ BSProject/bslibrary/src/main/java/com/artlite/bslibrary/helpers/contact/BSContactHelper.java
@@ -1,12 +1,25 @@
 package com.artlite.bslibrary.helpers.contact;
 
+import android.annotation.SuppressLint;
+import android.content.ContentProviderOperation;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresPermission;
+import android.util.Log;
 
+import com.artlite.bslibrary.annotations.Warning;
 import com.artlite.bslibrary.helpers.abs.BSBaseHelper;
+import com.artlite.bslibrary.managers.BSContextManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.WRITE_CONTACTS;
 
 /**
  * Class which provide the contact functionality
@@ -131,13 +144,191 @@ public final class BSContactHelper extends BSBaseHelper {
     }
 
     /**
+     * Class which provide the create of the contact builder
+     */
+    public static class ContactBuilder {
+
+        /**
+         * {@link String} constants of the tag
+         */
+        private static final String TAG = ContactBuilder.class.getSimpleName();
+
+        /**
+         * Instance of the {@link Context}
+         */
+        private final Context context;
+
+        /**
+         * Instance of the {@link List}
+         */
+        private final ArrayList contentProviderOperations;
+
+        /**
+         * Method which provide the creating of the {@link ContactBuilder}
+         *
+         * @param context instance of the {@link Context}
+         */
+        public ContactBuilder(Context context) {
+            this.context = context;
+            this.contentProviderOperations = new ArrayList();
+            //insert raw contact using RawContacts.CONTENT_URI
+            this.contentProviderOperations.add(ContentProviderOperation
+                    .newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                    .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                    .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                    .build());
+        }
+
+        /**
+         * Method which provide the setting of the {@link Object}
+         *
+         * @param value instance of the {@link Object}
+         * @return instance of the {@link ContactBuilder}
+         */
+        public ContactBuilder setName(@NonNull String value) {
+            if (value != null) {
+                //insert contact display name using Data.CONTENT_URI
+                contentProviderOperations.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                                value)
+                        .build());
+            }
+            return this;
+        }
+
+        /**
+         * Method which provide the setting of the {@link Object}
+         *
+         * @param value instance of the {@link Object}
+         * @return instance of the {@link ContactBuilder}
+         */
+        public ContactBuilder setNumber(@NonNull String value) {
+            if (value != null) {
+                //insert mobile number using Data.CONTENT_URI
+                contentProviderOperations.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, value)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                                ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                        .build());
+            }
+            return this;
+        }
+
+        /**
+         * Method which provide the setting of the {@link Object}
+         *
+         * @param value instance of the {@link Object}
+         * @return instance of the {@link ContactBuilder}
+         */
+        public ContactBuilder setCompany(@NonNull String value) {
+            if (value != null) {
+                //insert mobile number using Data.CONTENT_URI
+                contentProviderOperations.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, value)
+                        .build());
+            }
+            return this;
+        }
+
+        /**
+         * Method which provide the setting of the {@link Object}
+         *
+         * @param value instance of the {@link Object}
+         * @return instance of the {@link ContactBuilder}
+         */
+        public ContactBuilder setNote(@NonNull String value) {
+            if (value != null) {
+                //insert mobile number using Data.CONTENT_URI
+                contentProviderOperations.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Note.NOTE, value)
+                        .build());
+            }
+            return this;
+        }
+
+        /**
+         * Method which provide the setting of the {@link Object}
+         *
+         * @param value instance of the {@link Object}
+         * @return instance of the {@link ContactBuilder}
+         */
+        public ContactBuilder setEmail(@NonNull String value) {
+            if (value != null) {
+                //insert mobile number using Data.CONTENT_URI
+                contentProviderOperations.add(ContentProviderOperation
+                        .newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE,
+                                ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Email.ADDRESS, value)
+                        .build());
+            }
+            return this;
+        }
+
+        /**
+         * Method which provide the applying of the batch
+         *
+         * @return instance of the {@link Boolean}
+         */
+        @SuppressLint("MissingPermission")
+        @RequiresPermission(allOf = {READ_CONTACTS, WRITE_CONTACTS})
+        public boolean apply() {
+            try {
+                this.context.getContentResolver().applyBatch(ContactsContract.AUTHORITY,
+                        this.contentProviderOperations);
+                return true;
+            } catch (Exception ex) {
+                Log.e(TAG, "apply: ", ex);
+            }
+            return false;
+        }
+
+    }
+
+    /**
      * Method which provide the creating of the instance of the {@link ContactIntentBuilder}
      *
      * @return instance of the {@link ContactIntentBuilder}
      */
     @NonNull
-    public static ContactIntentBuilder create() {
+    public static ContactIntentBuilder createContactIntentBuilder() {
         return new ContactIntentBuilder();
+    }
+
+    /**
+     * Method which provide the creating of the instance of the {@link ContactIntentBuilder}
+     */
+    @Warning(massage = "For this method you need to initialize of the BSINStance")
+    @NonNull
+    public static ContactBuilder createContactBuilder() {
+        return new ContactBuilder(BSContextManager.getApplicationContext());
+    }
+
+    /**
+     * Method which provide the creating of the instance of the {@link ContactIntentBuilder}
+     *
+     * @return instance of the {@link ContactIntentBuilder}
+     */
+    @NonNull
+    public static ContactBuilder createContactBuilder(@NonNull Context context) {
+        return new ContactBuilder(context);
     }
 
 }
